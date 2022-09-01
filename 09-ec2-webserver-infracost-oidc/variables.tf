f77084d9ab5504dc87967a72d637157c96eb09c7
infratidev <infrati@infrati.dev>
2022-09-01 18:48:47 -0300

    feat: First commit

---
+++ 09-ec2-webserver-infracost-oidc/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+    description = "Default region"
+    default = "us-east-1"
+}
+variable "instance_type" {
+    description = "Default Instance"
+    default     =  "t2.nano"
+}
+
+variable "vpc_cidr" {
+    description = "VPC InfraTI"
+    default = "178.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "Public Subnet InfraTI Web Server"
+    default = "178.0.10.0/24"
+}
+
+variable "env" {
+  description = "Ambiente"
+  default     = "Development"
+}
+
+variable "name" {
+  description = "Nome da aplicação"
+  default     = "infrati-ec2server01"
+}
+
