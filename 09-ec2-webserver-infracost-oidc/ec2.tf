7158979f6a040d6c67ea6203b265886910b98ea0
infratidev <infrati@infrati.dev>
2022-09-01 19:45:54 -0300

    feat: First commit

---
+++ 09-ec2-webserver-infracost-oidc/ec2.tf
@@ -0,0 +1,39 @@
+data "aws_ami" "debian" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["debian-10-amd64-20210208-542"]
+  }
+
+  owners = ["136693071363"] # Debian 10
+
+}
+
+resource "aws_instance" "web" {
+  ami             = data.aws_ami.debian.id 
+  subnet_id       = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.sg.id]
+  instance_type   = var.instance_type
+
+user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  sudo echo "<h1> Welcome to my terraform =>infracode.dev<= IP: $(curl -s ifconfig.me) </h1>" | sudo tee "/var/www/html/index.html"
+  echo "*** Completed Installing apache2"
+  EOF
+
+
+  volume_tags = {
+    Name = "web_instance"
+  } 
+
+ tags = {
+    Name        = var.name
+    Environment = var.env
+    Provisioner = "Terraform Infracost"
+   }
+
+}
